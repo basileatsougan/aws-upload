c7dbad68fdb27404f2e1791efbe7015e9c92ad3c
atsougan870@gmail.com <atsougan870@gmail.com>
2024-11-26 04:18:45 +0200

    Add UPLOAD FILE button using Pressable

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <Pressable
+        style={{
+          backgroundColor: "#0e45eb",
+          width: 120,
+          height: 50,
+          justifyContent: "center",
+        }}
+      >
+        <Text
+          style={{
+            fontsize: 15,
+            fontWeight: "bold",
+            color: "#fff",
+            marginLeft: 15,
+          }}
+        >
+          UPLOAD FILE
+        </Text>
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
